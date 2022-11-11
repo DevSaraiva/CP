385771ae1fd8fbf5cf1e43e775b3ce173b10114a
saraiva <bernardosaraiva1503@gmail.com>
2022-11-11 23:10:31 +0000

    meo wifi

--- include/utils.h
+++ include/utils.h
@@ -8,37 +8,20 @@
 
 
 
-#define N 10000000
-#define K 4
-
-
-/*
-
-typedef struct Vector {       
+typedef struct Point {       
     float x;
     float y;
-} vector;
+} point;
 
 
 typedef struct Cluster {       
     
     size_t actual_size;
-    float xSum;
-    float ySum;
-    vector centroid;
-
+    point centroid;
+    point sum;
+    
 } cluster;
 
-*/
-
-
-
-float euclideanDistance(float x, float y, float x1, float y1);
-//vector centroidCalculator(cluster cluster);
-//void add_cluster(vector v, cluster * c);
-//void freeClusterPoints(cluster * clusters);
-//void printPoints(vector * points);
-void resetClustersSize(int * actual_size,float * sum_cluster);
 
 
 #endif
